842f2df80d3a47b74a490414507f0a94ddbf6097
jmichellevr29 <michellevillacis@hotmail.com>
2024-06-04 19:56:08 -0500

    weather js

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -10,6 +10,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
