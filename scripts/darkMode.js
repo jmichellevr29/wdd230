6d6b51d93f51075047ed9979f5581d1086b9abf6
jmichellevr29 <michellevillacis@hotmail.com>
2024-06-06 20:08:59 -0500

    W05

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,14 +1,20 @@
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+document.addEventListener("DOMContentLoaded", () => {
+    const modeButton = document.querySelector("#mode");
+    const main = document.querySelector("main");
 
-modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("🕶️")) {
-        main.style.background = "#000";
-        main.style.color = "#fff";
-        modeButton.textContent = "🔆";
+    if (modeButton && main) {
+        modeButton.addEventListener("click", () => {
+            if (modeButton.textContent.includes("🕶️")) {
+                main.style.background = "#000";
+                main.style.color = "#fff";
+                modeButton.textContent = "🔆";
+            } else {
+                main.style.background = "#eee";
+                main.style.color = "#000";
+                modeButton.textContent = "🕶️";
+            }
+        });
     } else {
-        main.style.background = "#eee";
-        main.style.color = "#000";
-        modeButton.textContent = "🕶️";
+        console.error("Mode button or main element not found");
     }
 });
